f44e5a8897132f013d198570e3885b081159f7cb
Ertuğrul Erata <ertugrulerata@gmail.com>
2025-09-16 22:51:18 +0300

    add docker group

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here if you plan on using SSH to connect
       ];
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "docker" ];
     };
   };
 
